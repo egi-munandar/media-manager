a4fb28d5fc8f4a746e3ea14451b892bc07aa3900
egi-munandar <egi_munandar@outlook.com>
2024-11-12 15:54:57 +0700

    public thumbs folder

--- app/Console/Commands/ScanMedia.php
+++ app/Console/Commands/ScanMedia.php
@@ -31,6 +31,9 @@ class ScanMedia extends Command
         $files = Storage::disk('media')->files();
         $rows = MediaFile::selectRaw('concat(file_name,\'.\', extension) as fl')->pluck('fl')->toArray();
         $extensions = ['webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gif', 'gifv', 'mng', 'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm', 'asf', 'amv', 'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod'];
+        if (!file_exists(public_path('thumbs'))) {
+            mkdir(public_path('thumbs'));
+        }
         //add new record
         if ($files) {
             $this->line(count($files) . ' File(s) to process');
